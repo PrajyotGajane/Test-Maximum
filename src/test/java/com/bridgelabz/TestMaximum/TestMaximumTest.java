f244434dfe8980df6de5cd3aec0dc547c3079e4c
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-21 03:01:30 +0530

    Added: Test class and Main class

---
+++ src/test/java/com/bridgelabz/TestMaximum/TestMaximumTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.TestMaximum;
+
+import org.junit.Test;
+
+public class TestMaximumTest {
+      @Test
+      public void givenThreeIntegers_ShouldFind_Maximum() {
+            TestMaximum testMaximum = new TestMaximum();
+      }
+}
