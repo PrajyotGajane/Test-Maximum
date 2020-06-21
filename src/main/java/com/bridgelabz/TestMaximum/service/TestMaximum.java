f88dd7a60959961cc04a6d600d1b4ebd6da2a557
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-21 15:56:54 +0530

    Refactored generic method to take extra parameters

--- src/main/java/com/bridgelabz/TestMaximum/service/TestMaximum.java
+++ src/main/java/com/bridgelabz/TestMaximum/service/TestMaximum.java
@@ -4,21 +4,30 @@ public class TestMaximum <E extends Comparable<E>>{
       E firstValue;
       E secondValue;
       E thirdValue;
-      public TestMaximum(E firstValue, E secondValue, E thirdValue){
+      E[] moreParameters;
+      public TestMaximum(E firstValue, E secondValue, E thirdValue, E... moreParameters){
             this.firstValue = firstValue;
             this.secondValue = secondValue;
             this.thirdValue = thirdValue;
+            this.moreParameters= moreParameters;
       }
       public E findMaximum(){
-            return TestMaximum.findMaximum(firstValue,secondValue,thirdValue);
+            return TestMaximum.findMaximum(firstValue,secondValue,thirdValue,moreParameters);
       }
-      public static  < E extends Comparable > E findMaximum(E firstValue, E secondValue, E thirdValue){
+      public static  < E extends Comparable<E> > E findMaximum(E firstValue, E secondValue, E thirdValue,E... moreParameters){
             E maximum = firstValue;
             if ( secondValue.compareTo(maximum) > 0 ){
                   maximum = secondValue;
             }
             if ( thirdValue.compareTo(maximum) > 0 ){
                   maximum = thirdValue;
+            }
+            if (moreParameters.length != 0 ){
+                  for (E parameter: moreParameters){
+                        if ( parameter.compareTo(maximum) > 0 ){
+                              maximum = parameter;
+                        }
+                  }
             }
             return maximum;
       }
