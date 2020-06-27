295e7ce16bf7a8a877dd30ed71945d04b6a8a060
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-27 09:57:13 +0530

    Refactored: TestMaximum to take any number of parameters

--- src/main/java/com/bridgelabz/TestMaximum/service/TestMaximum.java
+++ src/main/java/com/bridgelabz/TestMaximum/service/TestMaximum.java
@@ -1,62 +1,25 @@
 package com.bridgelabz.TestMaximum.service;
+import java.util.Arrays;
 public class TestMaximum <E extends Comparable<E>>{
-      E firstValue;
-      E secondValue;
-      E thirdValue;
       E[] moreParameters;
-      /**
-       * parameterised constructor to initialize generic values
-       * @param firstValue
-       * @param secondValue
-       * @param thirdValue
-       * @param moreParameters
-       */
-      public TestMaximum(E firstValue, E secondValue, E thirdValue, E... moreParameters){
-            this.firstValue = firstValue;
-            this.secondValue = secondValue;
-            this.thirdValue = thirdValue;
-            this.moreParameters= moreParameters;
+      public TestMaximum(E... moreParameters){
+            this.moreParameters = moreParameters;
       }
       /**
-       * to call parametrised method findMaximum
-       * @return maximum value
+       * to sort in ascending order to find maximum value off the passed values
+       * @return maximum value found
        */
       public E findMaximum(){
-            return TestMaximum.findMaximum(firstValue,secondValue,thirdValue,moreParameters);
+            Arrays.sort(moreParameters);
+            printMax(moreParameters[moreParameters.length - 1]);
+            return moreParameters[moreParameters.length - 1];
       }
       /**
-       * to check maximum value of Integer, String, Float with one generic method
-       * @param firstValue
-       * @param secondValue
-       * @param thirdValue
-       * @param moreParameters
-       * @param <E>
-       * @return maximum value
-       */
-      public static  < E extends Comparable<E> > E findMaximum(E firstValue, E secondValue, E thirdValue,E... moreParameters){
-            E maximum = firstValue;
-            if ( secondValue.compareTo(maximum) > 0 ){
-                  maximum = secondValue;
-            }
-            if ( thirdValue.compareTo(maximum) > 0 ){
-                  maximum = thirdValue;
-            }
-            if (moreParameters.length != 0 ){
-                  for (E parameter: moreParameters){
-                        if ( parameter.compareTo(maximum) > 0 ){
-                              maximum = parameter;
-                        }
-                  }
-            }
-            printMax(maximum);
-            return maximum;
-      }
-      /**
-       * to print maximum value passed by findMaximum
+       * to print the maximum value found
        * @param maximum
        * @param <E>
        */
-      public static <E extends Comparable<E>> void printMax(E maximum){
+      public static <E> void printMax(E maximum){
             System.out.println("Maximum value is :"+maximum);
       }
 }
